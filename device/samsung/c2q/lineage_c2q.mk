fa44444f4b7e33701b5c4f7b5083764a68187953
astardeviluke <devilukedxd@gmail.com>
2025-04-21 16:12:51 +0300

    Added necessary files and proprietary binaries for Samsung c2q

---
+++ device/samsung/c2q/lineage_c2q.mk
@@ -0,0 +1,9 @@
+PRODUCT_NAME := lineage_c2q
+PRODUCT_DEVICE := c2q
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := Galaxy Note 20 Ultra
+
+PRODUCT_PACKAGES += \
+    libcamera \
+    libexynoscamera_plugin \
+    libcamera2ndk_vendor
